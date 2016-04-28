a8cac32ebd15b6923e0c5c943a364ff9a703ffc6
Thomas de Graaff <t.de.graaff@vu.nl>
2016-04-28 15:23:40 +0200

    Update r files

--- Analysis.R
+++ Analysis.R
@@ -15,9 +15,32 @@ m6 <- plm(logemprate~logyouthshare:lmr_id, data = EmpData, effect = "twoways",in
 m7 <- lm(logunemprate~logyouthshare + factor(lmr_id) + factor(year), data = EmpData, weights=popshare)
 m8 <- ivreg(logunemprate~logyouthshare + factor(year)+factor(lmr_id)|.-logyouthshare+loginstrument, data = EmpData, weights=popshare) 
 
-# Heterogeneous impact
+# Heterogeneous impact of ethnicity
 
 m9 <- ivreg(logforemprate~logyouthshare + factor(year)+factor(lmr_id)|.-logyouthshare+loginstrument, data = EmpData, weights=popshare) 
+m10 <- ivreg(loggeremprate~logyouthshare + factor(year)+factor(lmr_id)|.-logyouthshare+loginstrument, data = EmpData, weights=popshare) 
+
+# Heterogeneous impact of age cohort
+
+m11 <- ivreg(logemprate2030~logyouthshare + factor(year)+factor(lmr_id)|.-logyouthshare+loginstrument, data = EmpData, weights=popshare) 
+m12 <- ivreg(logemprate3040~logyouthshare + factor(year)+factor(lmr_id)|.-logyouthshare+loginstrument, data = EmpData, weights=popshare) 
+m13 <- ivreg(logemprate4050~logyouthshare + factor(year)+factor(lmr_id)|.-logyouthshare+loginstrument, data = EmpData, weights=popshare) 
+m14 <- ivreg(logemprate5065~logyouthshare + factor(year)+factor(lmr_id)|.-logyouthshare+loginstrument, data = EmpData, weights=popshare) 
+
+# Heterogeneous impact by task
+
+m15 <- ivreg(logemptask1~logyouthshare + factor(year)+factor(lmr_id)|.-logyouthshare+loginstrument, data = EmpData, weights=popshare) 
+m16 <- ivreg(logemptask2~logyouthshare + factor(year)+factor(lmr_id)|.-logyouthshare+loginstrument, data = EmpData, weights=popshare) 
+m17 <- ivreg(logemptask3~logyouthshare + factor(year)+factor(lmr_id)|.-logyouthshare+loginstrument, data = EmpData, weights=popshare) 
+m18 <- ivreg(logemptask4~logyouthshare + factor(year)+factor(lmr_id)|.-logyouthshare+loginstrument, data = EmpData, weights=popshare) 
+m19 <- ivreg(logemptask5~logyouthshare + factor(year)+factor(lmr_id)|.-logyouthshare+loginstrument, data = EmpData, weights=popshare) 
+m20 <- ivreg(logemptask6~logyouthshare + factor(year)+factor(lmr_id)|.-logyouthshare+loginstrument, data = EmpData, weights=popshare) 
+m21 <- ivreg(logemptask7~logyouthshare + factor(year)+factor(lmr_id)|.-logyouthshare+loginstrument, data = EmpData, weights=popshare) 
+m22 <- ivreg(logemptask8~logyouthshare + factor(year)+factor(lmr_id)|.-logyouthshare+loginstrument, data = EmpData, weights=popshare) 
+m23 <- ivreg(logemptask9~logyouthshare + factor(year)+factor(lmr_id)|.-logyouthshare+loginstrument, data = EmpData, weights=popshare) 
+m24 <- ivreg(logemptask10~logyouthshare + factor(year)+factor(lmr_id)|.-logyouthshare+loginstrument, data = EmpData, weights=popshare) 
+m25 <- ivreg(logemptask11~logyouthshare + factor(year)+factor(lmr_id)|.-logyouthshare+loginstrument, data = EmpData, weights=popshare) 
+m26 <- ivreg(logemptask12~logyouthshare + factor(year)+factor(lmr_id)|.-logyouthshare+loginstrument, data = EmpData, weights=popshare) 
 
 ############### Get robust standard errors with default HC3 method (different than Stata who uses HC1 method) #######
 
@@ -26,12 +49,47 @@ robust_se_m5 <- sqrt(diag(vcovHC(m5, type = "HC3")))
 robust_se_m7 <- sqrt(diag(vcovHC(m7, type = "HC3")))
 robust_se_m8 <- sqrt(diag(vcovHC(m8, type = "HC3")))
 robust_se_m9 <- sqrt(diag(vcovHC(m9, type = "HC3")))
+robust_se_m10 <- sqrt(diag(vcovHC(m10, type = "HC3")))
+robust_se_m11 <- sqrt(diag(vcovHC(m11, type = "HC3")))
+robust_se_m12 <- sqrt(diag(vcovHC(m12, type = "HC3")))
+robust_se_m13 <- sqrt(diag(vcovHC(m13, type = "HC3")))
+robust_se_m14 <- sqrt(diag(vcovHC(m14, type = "HC3")))
+robust_se_m15 <- sqrt(diag(vcovHC(m15, type = "HC3")))
+robust_se_m16 <- sqrt(diag(vcovHC(m16, type = "HC3")))
+robust_se_m17 <- sqrt(diag(vcovHC(m17, type = "HC3")))
+robust_se_m18 <- sqrt(diag(vcovHC(m18, type = "HC3")))
+robust_se_m19 <- sqrt(diag(vcovHC(m19, type = "HC3")))
+robust_se_m20 <- sqrt(diag(vcovHC(m20, type = "HC3")))
+robust_se_m21 <- sqrt(diag(vcovHC(m21, type = "HC3")))
+robust_se_m22 <- sqrt(diag(vcovHC(m22, type = "HC3")))
+robust_se_m23 <- sqrt(diag(vcovHC(m23, type = "HC3")))
+robust_se_m24 <- sqrt(diag(vcovHC(m24, type = "HC3")))
+robust_se_m25 <- sqrt(diag(vcovHC(m25, type = "HC3")))
+robust_se_m26 <- sqrt(diag(vcovHC(m26, type = "HC3")))
+
 coefm2 <- coeftest(m2, vcov=vcovHC)
 coefm4 <- coeftest(m4, vcovHC(m4,"HC3"))
 coefm5 <- coeftest(m5, vcovHC(m5,"HC3"))
 coefm7 <- coeftest(m7, vcovHC(m7,"HC3"))
 coefm8 <- coeftest(m8, vcovHC(m8,"HC3"))
 coefm9 <- coeftest(m9, vcovHC(m9,"HC3"))
+coefm10 <- coeftest(m10, vcovHC(m10,"HC3"))
+coefm11 <- coeftest(m11, vcovHC(m11,"HC3"))
+coefm12 <- coeftest(m12, vcovHC(m12,"HC3"))
+coefm13 <- coeftest(m13, vcovHC(m13,"HC3"))
+coefm14 <- coeftest(m14, vcovHC(m14,"HC3"))
+coefm15 <- coeftest(m15, vcovHC(m15,"HC3"))
+coefm16 <- coeftest(m16, vcovHC(m16,"HC3"))
+coefm17 <- coeftest(m17, vcovHC(m17,"HC3"))
+coefm18 <- coeftest(m18, vcovHC(m18,"HC3"))
+coefm19 <- coeftest(m19, vcovHC(m19,"HC3"))
+coefm20 <- coeftest(m20, vcovHC(m20,"HC3"))
+coefm21 <- coeftest(m21, vcovHC(m21,"HC3"))
+coefm22 <- coeftest(m22, vcovHC(m22,"HC3"))
+coefm23 <- coeftest(m23, vcovHC(m23,"HC3"))
+coefm24 <- coeftest(m24, vcovHC(m24,"HC3"))
+coefm25 <- coeftest(m25, vcovHC(m25,"HC3"))
+coefm26 <- coeftest(m26, vcovHC(m26,"HC3"))
 
 ############### Write results to Latex file ###########
 # Be careful with overwriting (all additional fixed effects are
@@ -90,41 +148,41 @@ betacoef$id <- betacoef$clustermean.id
 
 ##################### Read in the shape file #################################################
 
-RegionData <- filter(EmpData,year==2010) # Just pick one year
-Regions <- readOGR(dsn = "Data/AMR_2012", layer = "AMR_2012") # Read in shape file
-centroids <- as.data.frame(coordinates(Regions)) # Get centroids of regions for labels 
-names(centroids) <- c("Longitude", "Latitude")  # Rename x and y coordinates
-centroids$id <- Regions$lmr2_id  #  Generate id variable
-
-Regions <- fortify(Regions, region="lmr2_id") # Create large data frame from shape file
-Regions$id <- strtoi(Regions$id, base = 0L)   # id should be an integer and not a string
-RegionData$id <- RegionData$lmr_id # Generate ID variable for database
-
-RegionData <- left_join(RegionData, centroids, by="id") # Join database with centroids
-RegionData <- left_join(RegionData, clustermean, by="id") # Join database with clustermean
-RegionData <- left_join(RegionData, betacoef, by="id")
-Regions <- left_join(Regions, RegionData, by="id") # Join shape file with database
-
-###################### Create general lay-out for figures #####################################
-
-p <- ggplot() + scale_fill_distiller(palette = "Greens", breaks = pretty_breaks(n = Nocluster-1), direction=1) 
-p <- p + guides(fill = guide_legend(reverse = TRUE))
-p <- p + theme_nothing(legend=TRUE)
-p <- p + theme(plot.title = element_text(size = rel(2), colour = "black")) 
-
-p1 <- ggplot() + scale_fill_distiller(palette = "PRGn", breaks = pretty_breaks(n = 10), direction=1) 
-p1 <- p1 + guides(fill = guide_legend(reverse = TRUE))
-p1 <- p1 + theme_nothing(legend=TRUE)
-p1 <- p1 + theme(plot.title = element_text(size = rel(2), colour = "black")) 
-
-######################## Create maps ###########################
-
-p_clusters <- p +  geom_polygon(data=Regions, aes(x= long, y = lat, group = group, fill = clustermean), color = "black", size = 0.25) + 
-    labs(title = "Spatial distribution of clusters across Germany", fill = "") + 
-    geom_text(data=RegionData, aes(label = substr(lmr_name,1,3), x = Longitude, y = Latitude))
-ggsave(filename = "Figs/Clusters.pdf", width = 9.65, height = 11)
-
-p_betacoef <- p1 +  geom_polygon(data=Regions, aes(x= long, y = lat, group = group, fill = betacoef), color = "black", size = 0.25) + 
-    labs(title = "Spatial distribution of beta coefficients across Germany", fill = "") + 
-    geom_text(data=RegionData, aes(label = substr(lmr_name,1,3), x = Longitude, y = Latitude))
-ggsave(filename = "Figs/BetaMap.pdf", width = 9.65, height = 11)
+# RegionData <- filter(EmpData,year==2010) # Just pick one year
+# Regions <- readOGR(dsn = "Data/AMR_2012", layer = "AMR_2012") # Read in shape file
+# centroids <- as.data.frame(coordinates(Regions)) # Get centroids of regions for labels 
+# names(centroids) <- c("Longitude", "Latitude")  # Rename x and y coordinates
+# centroids$id <- Regions$lmr2_id  #  Generate id variable
+# 
+# Regions <- fortify(Regions, region="lmr2_id") # Create large data frame from shape file
+# Regions$id <- strtoi(Regions$id, base = 0L)   # id should be an integer and not a string
+# RegionData$id <- RegionData$lmr_id # Generate ID variable for database
+# 
+# RegionData <- left_join(RegionData, centroids, by="id") # Join database with centroids
+# RegionData <- left_join(RegionData, clustermean, by="id") # Join database with clustermean
+# RegionData <- left_join(RegionData, betacoef, by="id")
+# Regions <- left_join(Regions, RegionData, by="id") # Join shape file with database
+# 
+# ###################### Create general lay-out for figures #####################################
+# 
+# p <- ggplot() + scale_fill_distiller(palette = "Greens", breaks = pretty_breaks(n = Nocluster-1), direction=1) 
+# p <- p + guides(fill = guide_legend(reverse = TRUE))
+# p <- p + theme_nothing(legend=TRUE)
+# p <- p + theme(plot.title = element_text(size = rel(2), colour = "black")) 
+# 
+# p1 <- ggplot() + scale_fill_distiller(palette = "PRGn", breaks = pretty_breaks(n = 10), direction=1) 
+# p1 <- p1 + guides(fill = guide_legend(reverse = TRUE))
+# p1 <- p1 + theme_nothing(legend=TRUE)
+# p1 <- p1 + theme(plot.title = element_text(size = rel(2), colour = "black")) 
+# 
+# ######################## Create maps ###########################
+# 
+# p_clusters <- p +  geom_polygon(data=Regions, aes(x= long, y = lat, group = group, fill = clustermean), color = "black", size = 0.25) + 
+#     labs(title = "Spatial distribution of clusters across Germany", fill = "") + 
+#     geom_text(data=RegionData, aes(label = substr(lmr_name,1,3), x = Longitude, y = Latitude))
+# ggsave(filename = "Figs/Clusters.pdf", width = 9.65, height = 11)
+# 
+# p_betacoef <- p1 +  geom_polygon(data=Regions, aes(x= long, y = lat, group = group, fill = betacoef), color = "black", size = 0.25) + 
+#     labs(title = "Spatial distribution of beta coefficients across Germany", fill = "") + 
+#     geom_text(data=RegionData, aes(label = substr(lmr_name,1,3), x = Longitude, y = Latitude))
+# ggsave(filename = "Figs/BetaMap.pdf", width = 9.65, height = 11)
--- ReadData.R
+++ ReadData.R
@@ -62,13 +62,39 @@ EmpData <- EmpData %>%
             pop_for_30_34 + pop_for_35_39 + pop_for_40_44 + 
             pop_for_45_49 + pop_for_50_54 + pop_for_55_59 + 
             pop_for_60_64,
+        Workingpopger = pop_ger_20_24 + pop_ger_25_29 + 
+            pop_ger_30_34 + pop_ger_35_39 + pop_ger_40_44 + 
+            pop_ger_45_49 + pop_ger_50_54 + pop_ger_55_59 + 
+            pop_ger_60_64,
+        Emp2030 = emp_20_24 + emp_25_29,
+        Emp3040 = emp_30_34 + emp_35_39,
+        Emp4050 = emp_40_44 + emp_45_49,
+        Emp5065 = emp_50_54 + emp_55_59 + emp_65,
+        logemprate2030 = log((Emp2030)/(pop_20_24 + pop_25_29)),
+        logemprate3040 = log((Emp3040)/(pop_30_34 + pop_35_39)),
+        logemprate4050 = log((Emp4050)/(pop_40_44 + pop_45_49)),
+        logemprate5065 = log((Emp5065)/(pop_50_54 + pop_55_59 + pop_60_64)),        
+        logemptask1 = emp_task_1/emp,
+        logemptask2 = emp_task_2/emp,
+        logemptask3 = emp_task_3/emp,
+        logemptask4 = emp_task_4/emp,
+        logemptask5 = emp_task_5/emp,
+        logemptask6 = emp_task_6/emp,
+        logemptask7 = emp_task_7/emp,
+        logemptask8 = emp_task_8/emp,
+        logemptask9 = emp_task_9/emp,
+        logemptask10 = emp_task_10/emp,
+        logemptask11 = emp_task_11/emp,
+        logemptask12 = emp_task_12/emp,
         unemprate = unemp/Workingpop,
         emprate = emp/Workingpop,        
         foremprate = emp_for/Workingpopfor,
+        geremprate = emp_ger/Workingpopger,
         participationrate = (unemp + emp)/Workingpop,
         youthshare = (pop_18_19 + pop_20_24)/Workingpop,
         popshare = pop/sum(pop),
         logforemprate = log(foremprate),
+        loggeremprate = log(geremprate),
         logemprate = log(emprate),
         logunemprate = log(unemprate),
         logyouthshare = log(youthshare), 
@@ -97,47 +123,47 @@ ggplot(data = YearData, aes(x=year, y = meanemprate)) +geom_line()
 
 ############### Making descriptive maps #################
 
-RegionData <- filter(EmpData,year==2010) # Just pick one year
-Regions <- readOGR(dsn = "Data/AMR_2012", layer = "AMR_2012") # Read in shape file
-centroids <- as.data.frame(coordinates(Regions)) # Get centroids of regions for labels 
-names(centroids) <- c("Longitude", "Latitude")  # Rename x and y coordinates
-centroids$id <- Regions$lmr2_id  #  Generate id variable
-
-Regions <- fortify(Regions, region="lmr2_id") # Create large data frame from shape file
-Regions$id <- strtoi(Regions$id, base = 0L)   # id should be an integer and not a string
-RegionData$id <- RegionData$lmr_id # Generate ID variable for database
-
-RegionData <- left_join(RegionData, centroids, by="id") # Join database with centroids
-Regions <- left_join(Regions, RegionData, by="id") # Join shape file with database
-
-###################### Create general lay-out for figures #####################################
-
-p <- ggplot() + scale_fill_distiller(palette = "Greens", labels = percent, breaks = pretty_breaks(n = 10), direction=1) 
-p <- p + guides(fill = guide_legend(reverse = TRUE))
-p <- p + theme_nothing(legend=TRUE)
-p <- p + theme(plot.title = element_text(size = rel(2), colour = "black")) 
-
-###################### Make specific maps for different variables
-
-p_unem <- p +  geom_polygon(data=Regions, aes  (x= long, y = lat, group = group, fill = unemprate), color = "black", size = 0.25) + 
-    labs(title = "Percentage of unemployment in\n German labor market regions in 2010", fill = "") + 
-    geom_text(data=RegionData, aes(label = substr(lmr_name,1,3), x = Longitude, y = Latitude))
-ggsave(filename = "Figs/UnemploymentRate.pdf", width = 9.65, height = 11)
-
-p_em <- p +  geom_polygon(data=Regions, aes  (x= long, y = lat, group = group, fill = emprate), color = "black", size = 0.25) + 
-    labs(title = "Percentage of employment in\n German labor market regions in 2010", fill = "") + 
-    geom_text(data=RegionData, aes(label = substr(lmr_name,1,3), x = Longitude, y = Latitude))
-ggsave(filename = "Figs/EmploymentRate.pdf", width = 9.65, height = 11)
-
-p_youth <- p +  geom_polygon(data=Regions, aes  (x= long, y = lat, group = group, fill = youthshare), color = "black", size = 0.25) + 
-    labs(title = "Percentage of employment in\n German labor market regions in 2010", fill = "") + 
-    geom_text(data=RegionData, aes(label = substr(lmr_name,1,3), x = Longitude, y = Latitude))
-ggsave(filename = "Figs/YouthShare.pdf", width = 9.65, height = 11)
-
-p_pr <- p +  geom_polygon(data=Regions, aes  (x= long, y = lat, group = group, fill = participationrate), color = "black", size = 0.25) + 
-    labs(title = "Participation rate in\n German labor market regions in 2010", fill = "") + 
-    geom_text(data=RegionData, aes(label = substr(lmr_name,1,3), x = Longitude, y = Latitude))
-ggsave(filename = "Figs/ParticipationRate.pdf", width = 9.65, height = 11)
+# RegionData <- filter(EmpData,year==2010) # Just pick one year
+# Regions <- readOGR(dsn = "Data/AMR_2012", layer = "AMR_2012") # Read in shape file
+# centroids <- as.data.frame(coordinates(Regions)) # Get centroids of regions for labels 
+# names(centroids) <- c("Longitude", "Latitude")  # Rename x and y coordinates
+# centroids$id <- Regions$lmr2_id  #  Generate id variable
+# 
+# Regions <- fortify(Regions, region="lmr2_id") # Create large data frame from shape file
+# Regions$id <- strtoi(Regions$id, base = 0L)   # id should be an integer and not a string
+# RegionData$id <- RegionData$lmr_id # Generate ID variable for database
+# 
+# RegionData <- left_join(RegionData, centroids, by="id") # Join database with centroids
+# Regions <- left_join(Regions, RegionData, by="id") # Join shape file with database
+# 
+# ###################### Create general lay-out for figures #####################################
+# 
+# p <- ggplot() + scale_fill_distiller(palette = "Greens", labels = percent, breaks = pretty_breaks(n = 10), direction=1) 
+# p <- p + guides(fill = guide_legend(reverse = TRUE))
+# p <- p + theme_nothing(legend=TRUE)
+# p <- p + theme(plot.title = element_text(size = rel(2), colour = "black")) 
+# 
+# ###################### Make specific maps for different variables
+# 
+# p_unem <- p +  geom_polygon(data=Regions, aes  (x= long, y = lat, group = group, fill = unemprate), color = "black", size = 0.25) + 
+#     labs(title = "Percentage of unemployment in\n German labor market regions in 2010", fill = "") + 
+#     geom_text(data=RegionData, aes(label = substr(lmr_name,1,3), x = Longitude, y = Latitude))
+# ggsave(filename = "Figs/UnemploymentRate.pdf", width = 9.65, height = 11)
+# 
+# p_em <- p +  geom_polygon(data=Regions, aes  (x= long, y = lat, group = group, fill = emprate), color = "black", size = 0.25) + 
+#     labs(title = "Percentage of employment in\n German labor market regions in 2010", fill = "") + 
+#     geom_text(data=RegionData, aes(label = substr(lmr_name,1,3), x = Longitude, y = Latitude))
+# ggsave(filename = "Figs/EmploymentRate.pdf", width = 9.65, height = 11)
+# 
+# p_youth <- p +  geom_polygon(data=Regions, aes  (x= long, y = lat, group = group, fill = youthshare), color = "black", size = 0.25) + 
+#     labs(title = "Percentage of employment in\n German labor market regions in 2010", fill = "") + 
+#     geom_text(data=RegionData, aes(label = substr(lmr_name,1,3), x = Longitude, y = Latitude))
+# ggsave(filename = "Figs/YouthShare.pdf", width = 9.65, height = 11)
+# 
+# p_pr <- p +  geom_polygon(data=Regions, aes  (x= long, y = lat, group = group, fill = participationrate), color = "black", size = 0.25) + 
+#     labs(title = "Participation rate in\n German labor market regions in 2010", fill = "") + 
+#     geom_text(data=RegionData, aes(label = substr(lmr_name,1,3), x = Longitude, y = Latitude))
+# ggsave(filename = "Figs/ParticipationRate.pdf", width = 9.65, height = 11)
 
 ##################### Clean Up #####################
 
